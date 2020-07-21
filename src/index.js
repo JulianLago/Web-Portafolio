d1b1156a426051cb98119e3de4dfdbac992a0832
julian lago <julianlago93@gmail.com>
2020-07-21 18:18:48 -0300

    update.css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-function activaBoton(){
-    $('.boton').click(function(){
-        if($(this).hasClass('btn-cerrar')){
-        $('.boton').removeClass('btn-abrir'),
-        $('.boton').addClass('btn-cerrar'),
-        $(this).toggleClass('btn-cerrar btn-abrir')
-        
-    }
-    })};
+
 function muestraContenido(){
     $('.boton').click(function(){
         let nombreContenido = $(this).text();
@@ -16,53 +8,18 @@ function muestraContenido(){
 
   };
 
+  function responsiveBio(){
+    if (window.matchMedia('(max-width: 800px)').matches) {
+        $('#bio').insertBefore('#contenido');
+    }
+};
         /////////////////////////////////////////////////////////////
 
     $('.ventana-contenido').hide();
     $('#Proyectos').show();
     
-    activaBoton();
-    muestraContenido();
   
-    /*
-    $('.ventana-btn').click(function(){
-        if($(this).hasClass('btn-abrir')){
-            $('.ventana-conten').hide('400'),
-            $(this).parent().siblings('.ventana-conten').show('400')
-    
-        }else{
-            $(this).parent().siblings('.ventana-conten').hide('400'),
-            $(this).toggleClass("btn-cerrar btn-abrir")
-        }
-    });
-
-
-    $('.ventana-btn').click(function(){
-        let $boton = $(this);
-        $(this).toggleClass('btn-abrir btn-cerrar'),
-        $('.ventana-btn').toggleClass('btn-cerrar btn-abrir')
-
-    });
-
-    $('.ventana-btn').click(function(){
-        if($(this).hasClass("btn-abrir")){
-            $(this).siblings(".ventana-conten").show("400"),
-            $(this).toggleClass("btn-abrir btn-cerrar")
-        }else{
-            $(this).siblings(".ventana-conten").hide("400"),
-            $(this).toggleClass("btn-cerrar btn-abrir")
-        }
-    });
-*/
-    /*
-$('.btn-cerrar').click(function(){
-    $(this).siblings(".ventana-conten").hide("400")
-    $(this).removeClass("btn-cerrar"),
-    $(this).addClass("btn-abrir")
-});
-$('.btn-abrir').click(function(){
-    $(this).siblings(".ventana-conten").show("400")
-    $()
-
-});
-*/
+    muestraContenido();
+    responsiveBio();
+   
+   
